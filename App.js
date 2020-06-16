d5eb5a5d552671b9618e9679763bffe61f5c42bd
Ann Newcomer <a.f.newcomer@gmail.com>
2020-06-15 19:50:30 -0600

    Second Commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="Ann's Kitty-love logo" />
+    <h1>Ann's React App</h1>
+    </div>
+  );
+}
+
+export default App;
